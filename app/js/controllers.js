321bac1c660006cd9aa7c0bd86d0c4488746c128
David Young-Chan Kay <davidykay@gmail.com>
2013-01-06 20:54:27 -0500

    imported todo code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,12 +8,35 @@ function RecipeListCtrl($scope, Recipe) {
 }
 
 function RecipeNewCtrl($scope, Recipe) {
+  var recipes = $scope.recipes = recipeStorage.get();
+
+  $scope.newRecipe = "";
+  $scope.editedRecipe = null;
+
+  $scope.$watch('recipes', function() {
+    $scope.remainingCount = filterFilter(recipes, {completed: false}).length;
+    $scope.doneCount = recipes.length - $scope.remainingCount;
+    $scope.allChecked = !$scope.remainingCount
+    recipeStorage.put(recipes);
+  }, true);
+
+
+  $scope.addRecipe = function() {
+    if ( !$scope.newRecipe.length ) {
+      return;
+    }
+
+    recipes.push({
+      title: $scope.newRecipe,
+        completed: false
+    });
+
+    $scope.newRecipe = '';
+  };
 
 }
 
 //RecipeListCtrl.$inject = ['$scope', 'Recipe'];
-
-
 
 function RecipeDetailCtrl($scope, $routeParams, Recipe) {
   $scope.recipe = Recipe.get({recipeId: $routeParams.recipeId}, function(recipe) {
--- app/js/services.js
+++ app/js/services.js
@@ -3,8 +3,22 @@
 /* Services */
 
 angular.module('recipecatServices', ['ngResource']).
-    factory('Recipe', function($resource){
-  return $resource('recipes/:recipeId.json', {}, {
-    query: {method:'GET', params:{recipeId:'recipes'}, isArray:true}
+  factory('Recipe', function($resource){
+    return $resource('recipes/:recipeId.json', {}, {
+      query: {method:'GET', params:{recipeId:'recipes'}, isArray:true}
+    });
+
+  factory( 'recipeStorage', function() {
+    var STORAGE_ID = 'recipes-angularjs';
+
+    return {
+      get: function() {
+        return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+      },
+
+      put: function( recipes ) {
+        localStorage.setItem(STORAGE_ID, JSON.stringify(recipes));
+      }
+    };
   });
 });
